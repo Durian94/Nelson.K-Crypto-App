366188f97ab41b2c1b3876cd26769f66fc8e766e
Nelson Kelly <nelsonkellyliverpool@gmail.com>
2022-12-13 12:00:53 +1100

    added router/other dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route as Page
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from './App.styles';
+import Home from './Pages/Home/Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Page exact path="/" component={Home} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
